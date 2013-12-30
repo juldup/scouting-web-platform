70e1886ec3e3ec383474483b214096f3044cc843
Julien Dupuis <julien.dupuis@gmail.com>
2013-12-30 16:34:22 +0100

    Full list of privileges

--- app/models/Privilege.php
+++ app/models/Privilege.php
@@ -23,11 +23,16 @@ class Privilege extends Eloquent {
       'section' => false,
   );
   
-  // TODO ...
-  
   public static $EDIT_PAGES = array(
       'id' => 'Edit pages',
       'text' => 'Modifier les pages #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $EDIT_SECTION_EMAIL_ADDRESS = array(
+      'id' => "Edit section e-mail address",
+      'text' => "Changer l'adresse e-mail #delasection",
       'unit' => true,
       'section' => true,
   );
@@ -38,133 +43,142 @@ class Privilege extends Eloquent {
       'unit' => true,
       'section' => true,
   );
-
-
-
-
-
-
-  /*     
-  $operationsBase = Array(
-    "Privilèges de base des animateurs" =>
-      Array(
-        "U:Accéder au coin des animateurs",
-        "U:",
-        "U:",
-        "U:")
-    );
-  $operations = Array(
-    "Voir les données" =>
-      Array(
-        "S:Voir le listing complet #delasection",
-        "S:Consulter les fiches santé #delasection",
-        "S:Voir l'état des comptes #delasection",
-        "S:Voir les changements du site concernant #lasection"),
-    "Calendrier" =>
-      Array(
-        "S:Modifier les entrées du calendrier #delasection"),
-    "Photos" =>
-      Array(
-        "S:Ajouter/supprimer des photos pour #lasection"),
-    "Documents" =>
-      Array(
-        "S:Modifier les documents #delasection"),
-    "Covoiturage" =>
-      Array(
-        "S:Gérer le covoiturage #delasection"),
-    "Infos de la section" =>
-      Array(
-        "S:Modifier les pages #delasection",
-        "S:Changer l'adresse e-mail #delasection",
-        "S:Poster des nouvelles pour #lasection"),
-    "E-mails" =>
-      Array(
-        "S:Envoyer des e-mails aux membres #delasection"),
-    "Trésorerie" =>
-      Array(
-        "S:Gérer les comptes #delasection"),
-    "Animateurs" =>
-      Array(
-        "S:Modifier les animateurs #delasection",
-        "S:Changer les privilèges des animateurs #delasection"),
-    "Fiches santé" =>
-      Array(
-        "S:Supprimer des fiches santé #delasection"),
-    "Listing" =>
-      Array(
-        "S:Modifier certaines données du listing #delasection",
-        "S:Modifier les données sensibles du listing #delasection",
-        "S:Changer des scouts de ou vers #lasection",
-        "S:Augmenter l'année des scouts #delasection",
-        "S:Valider les inscriptions #delasection",
-        "S:Changer un scout en animateur #delasection")
-    );
   
-  $operationsUnite = Array(
-    "Privilèges de base des animateurs" =>
-      Array(
-        "U:Voir le listing complet #delasection",
-        "U:Voir les documents partagés entre animateurs #delasection",
-        "U:Consulter les fiches santé #delasection",
-        "U:Voir l'état des comptes #delasection",
-        "U:Voir les changements du site concernant #lasection"),
-    "Calendrier" =>
-      Array(
-        "U:Modifier les entrées du calendrier #delasection"),
-    "Photos" =>
-      Array(
-        "U:Ajouter/supprimer des photos pour #lasection"),
-    "Documents" =>
-      Array(
-        "U:Modifier les documents #delasection"),
-    "Covoiturage" =>
-      Array(
-        "U:Gérer le covoiturage #delasection"),
-    "Infos du site" =>
-      Array(
-        "U:Modifier les pages #delasection",
-        "U:Changer l'adresse e-mail #delasection",
-        "U:Poster des nouvelles pour #lasection"),
-    "E-mails" =>
-      Array(
-        "U:Envoyer des e-mails aux membres #delasection"),
-    "Trésorerie" =>
-      Array(
-        "U:Gérer les comptes #delasection",
-        "U:Modifier le statut de paiement"),
-    "Animateurs" =>
-      Array(
-        "U:Modifier les animateurs #delasection",
-        "U:Changer les privilèges des animateurs #delasection"),
-    "Fiches santé" =>
-      Array(
-        "U:Supprimer des fiches santé #delasection"),
-    "Listing" =>
-      Array(
-        "U:Modifier certaines données du listing #delasection",
-        "U:Modifier les données sensibles du listing #delasection",
-        "U:Changer des scouts de ou vers #lasection",
-        "U:Augmenter l'année des scouts #delasection",
-        "U:Valider les inscriptions #delasection",
-        "U:Changer un scout en animateur #delasection",
-        "U:Exporter le listing d'unité pour la fédération"),
-    "Fête d'unité" =>
-      Array(
-        "U:Valider les inscriptions pour la fête d'unité"),
-    "Opérations avancées" =>
-      Array(
-        "U:Supprimer des documents partagés entre animateurs #delasection",
-        "U:Supprimer des suggestions",
-        "U:Répondre aux suggestions",
-        "U:Changer les paramètres du site",
-        "U:Créer et renommer des sections",
-        "U:Changer les codes des sections"),
-    "Gestion du site avancée (Webmaster)" =>
-      Array(
-        "U:Modifier les couleurs des sections",
-        "U:Supprimer des comptes d'utilisateurs",
-        "U:Supprimer des entrées du livre d'or",
-        "U:Voir les logs")
+  public static $EDIT_NEWS = array(
+      'id' => "Edit news",
+      'text' => 'Poster des nouvelles pour #lasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $EDIT_DOCUMENTS = array(
+      'id' => "Edit documents",
+      'text' => 'Modifier les documents #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $SEND_EMAILS = array(
+      'id' => "Send e-mails",
+      'text' => 'Envoyer des e-mails aux membres #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $POST_PHOTOS = array(
+      'id' => "Post photos",
+      'text' => 'Ajouter/supprimer des photos pour #lasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $MANAGE_ACCOUNTS = array(
+      'id' => "Manage accounts",
+      'text' => 'Gérer les comptes #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $EDIT_LISTING_LIMITED = array(
+      'id' => "Edit listing limited",
+      'text' => 'Modifier certaines données du listing #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $EDIT_LISTING_ALL = array(
+      'id' => "Edit listing all",
+      'text' => 'Modifier les données sensibles du listing #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $VIEW_HEALTH_CARDS = array(
+      'id' => "Edit listing all",
+      'text' => 'Consulter les fiches santé #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $DELETE_HEALTH_CARDS = array(
+      'id' => "Edit listing all",
+      'text' => 'Supprimer des fiches santé #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $EDIT_LEADER_PRIVILEGES = array(
+      'id' => "Edit leader privileges",
+      'text' => 'Changer les privilèges des animateurs #delasection',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $UPDATE_PAYMENT_STATUS = array(
+      'id' => "Update payment status",
+      'text' => 'Modifier le statut de paiement',
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $MANAGE_ANNUAL_FEAST_REGISTRATION = array(
+      'id' => "Manage annual feast registration",
+      'text' => "Valider les inscriptions pour la fête d'unité",
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $MANAGE_SUGGESIONS = array(
+      'id' => "Manage suggestions",
+      'text' => "Répondre aux suggestions et les supprimer",
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $EDIT_GLOBAL_PARAMETERS = array(
+      'id' => "Edit global parameters",
+      'text' => "Changer les paramètres du site",
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $MANAGE_SECTIONS = array(
+      'id' => "Manage sections",
+      'text' => "Créer, modifier et supprimer les sections",
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $DELETE_USERS = array(
+      'id' => "Delete users",
+      'text' => "Supprimer des comptes d'utilisateurs",
+      'unit' => true,
+      'section' => true,
+  );
+  
+  public static $DELETE_GUEST_BOOK_ENTRIES = array(
+      'id' => "Delete guest book entries",
+      'text' => "Supprimer des entrées du livre d'or",
+      'unit' => true,
+      'section' => true,
+  );
+
+
+  /*
+        "U:Accéder au coin des animateurs", => everybody
+        "S:Voir le listing complet #delasection", => everybody
+        "S:Voir l'état des comptes #delasection", => everybody
+        "S:Voir les changements du site concernant #lasection"), => everybody
+        "S:Changer des scouts de ou vers #lasection", => listing_all
+        "S:Augmenter l'année des scouts #delasection", => listing_limited
+        "S:Valider les inscriptions #delasection", => listing_all
+        "S:Changer un scout en animateur #delasection") => listing_all
+        "U:Voir les documents partagés entre animateurs #delasection", => feature no longer supported
+        "U:Gérer le covoiturage #delasection"), => feature no longer supported
+        "U:Exporter le listing d'unité pour la fédération"), => feature maybe no longer supported
+        "U:Changer les codes des sections"), => manage sections
+        "U:Modifier les couleurs des sections", => manage sections
+        "U:Voir les logs") => feature no longer supported
     );
 */
   
