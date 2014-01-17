985478843d5e46b648670ec482ba5efc8c4248aa
Julien Dupuis <julien.dupuis@gmail.com>
2014-01-17 15:31:14 +0100

    Links to personal e-mails in leader page

--- app/views/pages/leader/leaders.blade.php
+++ app/views/pages/leader/leaders.blade.php
@@ -32,74 +32,83 @@
     </div>
   </div>
   
-  <?php $currentLeaderInCharge = false; ?>
-  @foreach ($leaders as $leader)
-    @if ($currentLeaderInCharge != $leader->leader_in_charge)
-      <div class="row">
-        <div class="col-md-12">
-          <legend>
-            @if ($leader->leader_in_charge)
-              <?php $currentLeaderInCharge = true; ?>
-              @if ($user->currentSection->id == 1)
-                @if ($count_in_charge > 1 && $men_in_charge) Animateurs d'unité
-                @elseif ($count_in_charge > 1 && !$men_in_charge) Animatrices d'unité
-                @elseif ($men_in_charge) Animateur d'unité
-                @else Animatrice d'unité
-                @endif
-              @else
-                @if ($count_in_charge > 1 && $men_in_charge) Animateurs responsables
-                @elseif ($count_in_charge > 1 && !$men_in_charge) Animatrices responsables
-                @elseif ($men_in_charge) Animateur responsable
-                @else Animatrice responsable
-                @endif
-              @endif
-            @else
-              <?php $currentLeaderInCharge = false; ?>
-              @if ($user->currentSection->id == 1)
-                @if ($count_others > 1 && $men_in_others) Équipiers d'unité
-                @elseif ($count_others > 1 && !$men_in_others) Équipières d'unité
-                @elseif ($men_in_others) Équipier d'unité
-                @else Équipière d'unité
-                @endif
-              @else
-                @if ($count_others > 1 && $men_in_others) Équipiers
-                @elseif ($count_others > 1 && !$men_in_others) Équipières
-                @elseif ($men_in_others) Équipier
-                @else Équipière
-                @endif
-              @endif
-            @endif
-          </h3>
+  <div class="row">
+    <?php $currentLeaderInCharge = false; ?>
+    @foreach ($leaders as $leader)
+      @if ($currentLeaderInCharge != $leader->leader_in_charge)
         </div>
-      </div>
-    @endif
-    <div class="col-md-6 leader-card">
-      <div class="well">
         <div class="row">
-          <div class="col-md-6">
-            @if ($leader->has_picture)
-              <img class="leader_picture" src="{{ $leader->getPictureURL() }}" />
-            @else
-              <img class="leader_picture" src="" alt=" Pas de photo " />
-            @endif
+          <div class="col-md-12">
+            <legend>
+              @if ($leader->leader_in_charge)
+                <?php $currentLeaderInCharge = true; ?>
+                @if ($user->currentSection->id == 1)
+                  @if ($count_in_charge > 1 && $men_in_charge) Animateurs d'unité
+                  @elseif ($count_in_charge > 1 && !$men_in_charge) Animatrices d'unité
+                  @elseif ($men_in_charge) Animateur d'unité
+                  @else Animatrice d'unité
+                  @endif
+                @else
+                  @if ($count_in_charge > 1 && $men_in_charge) Animateurs responsables
+                  @elseif ($count_in_charge > 1 && !$men_in_charge) Animatrices responsables
+                  @elseif ($men_in_charge) Animateur responsable
+                  @else Animatrice responsable
+                  @endif
+                @endif
+              @else
+                <?php $currentLeaderInCharge = false; ?>
+                @if ($user->currentSection->id == 1)
+                  @if ($count_others > 1 && $men_in_others) Équipiers d'unité
+                  @elseif ($count_others > 1 && !$men_in_others) Équipières d'unité
+                  @elseif ($men_in_others) Équipier d'unité
+                  @else Équipière d'unité
+                  @endif
+                @else
+                  @if ($count_others > 1 && $men_in_others) Équipiers
+                  @elseif ($count_others > 1 && !$men_in_others) Équipières
+                  @elseif ($men_in_others) Équipier
+                  @else Équipière
+                  @endif
+                @endif
+              @endif
+            </legend>
           </div>
-          <div class="col-md-6">
-            <p class="leader-name">{{ $leader->leader_name }}</p>
-            <p class="leader-real-name">{{ $leader->first_name }} {{ $leader->last_name }}</p>
-            <p><em>{{ Helper::rawToHTML($leader->leader_description) }}</em></p>
-            @if ($leader->leader_role)
-              <p><strong>Rôle :</strong> {{ Helper::rawToHTML($leader->leader_role) }}</p>
-            @endif
-            @if (!$leader->phone_member_private && $leader->phone_member)
-              <p><strong>GSM :</strong> {{ $leader->phone_member }}</p>
-            @endif
-            @if ($leader->email_member)
-              <p><strong>E-mail :</strong> <a class="btn-sm btn-primary" href="">Envoyer un e-mail</a></p>
-            @endif
+        </div>
+        <div class="row">
+      @endif
+      <div class="col-md-6 leader-card">
+        <div class="well">
+          <div class="row">
+            <div class="col-md-6">
+              @if ($leader->has_picture)
+                <img class="leader_picture" src="{{ $leader->getPictureURL() }}" />
+              @else
+                <img class="leader_picture" src="" alt=" Pas de photo " />
+              @endif
+            </div>
+            <div class="col-md-6">
+              <p class="leader-name">{{ $leader->leader_name }}</p>
+              <p class="leader-real-name">{{ $leader->first_name }} {{ $leader->last_name }}</p>
+              <p><em>{{ Helper::rawToHTML($leader->leader_description) }}</em></p>
+              @if ($leader->leader_role)
+                <p><strong>Rôle :</strong> {{ Helper::rawToHTML($leader->leader_role) }}</p>
+              @endif
+              @if (!$leader->phone_member_private && $leader->phone_member)
+                <p><strong>GSM :</strong> {{ $leader->phone_member }}</p>
+              @endif
+              @if ($leader->email_member)
+                <p>
+                  <strong>E-mail :</strong>
+                  <a class="btn-sm btn-primary" href="{{ URL::route('personal_email', array('contact_type' => PersonalEmailController::$CONTACT_TYPE_PERSONAL, 'member_id' => $leader->id)) }}">
+                    Envoyer un e-mail
+                  </a>
+                </p>
+              @endif
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  @endforeach
+    @endforeach
+  </div>
   
 @stop
