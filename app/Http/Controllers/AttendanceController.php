8722db4d43bcbc9cf35395ded4a4d5bb5a2cce98
Julien Dupuis <julien.dupuis@gmail.com>
2025-02-28 15:45:04 +0100

    Fix attendance bug

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -264,7 +264,7 @@ class AttendanceController extends BaseController {
                 $attendance = Attendance::create(array(
                     'member_id' => $memberData->id,
                     'event_id' => $event->id,
-                    'section_id' => $this->user->currentSection->id,
+                    //'section_id' => $this->user->currentSection->id,
                     'attended' => $attended,
                 ));
                 if ($attended != 0) {
